addf002de89d609bc8b2f9d2896e2a5f988dc75a
alexxxshib <alexxxshib@gmail.com>
2015-02-26 23:08:44 +0600

    refactored code structure

---
+++ app/src/main/java/com/shibkov/tasknotebook/app/TaskNotebookActivity.java
@@ -0,0 +1,66 @@
+package com.shibkov.tasknotebook.app;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.shibkov.tasknotebook.app.views.widgets.SlidingTabLayout;
+import com.shibkov.tasknotebook.app.views.adapters.ViewPagerAdapter;
+
+
+public class TaskNotebookActivity extends ActionBarActivity {
+
+    private ViewPager pager;
+    private ViewPagerAdapter adapter;
+    private SlidingTabLayout tabs;
+    private CharSequence Titles[] = {"Home", "Events"};
+    private int Numboftabs = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_notebook);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.tool_bar));
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles, Numboftabs);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
+
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.accent);
+            }
+
+            @Override
+            public int getDividerColor(int position) {
+                return getResources().getColor(R.color.accent);//todo change color
+            }
+        });
+
+        tabs.setViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
